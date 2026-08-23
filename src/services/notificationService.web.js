1b046262aa789c62edf83899bcfc32e01b679152
Sergs Rafael Oriel <oriel.sergs@gmail.com>
2026-08-24 00:27:43 +0800

    feat: implement critical notifications, alert details, and nearby shelters

---
+++ src/services/notificationService.web.js
@@ -0,0 +1,5 @@
+export const prepareNotifications = async () => 'unsupported';
+export const requestNotificationPermission = async () => 'unsupported';
+export const showAlertNotification = async () => null;
+export const subscribeToNotificationResponses = () => ({ remove: () => {} });
+export const getLastNotificationAlertId = async () => null;
